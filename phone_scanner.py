e9a59d56e2735195837f38f55a2dfc1944ca2763
Samuel Havron <sgh7cc@virginia.edu>
2018-01-23 12:41:15 -0500

    some android tweaks

--- phone_scanner.py
+++ phone_scanner.py
@@ -7,8 +7,8 @@ import config
 '''
 NEED https://github.com/imkira/mobiledevice installed (`brew install mobiledevice` or build from source).
 '''
-def ios_scan(phone_id):
-   installed_apps = subprocess.run(['mobiledevice', 'list_apps', '-u', phone_id], stdout=subprocess.PIPE) 
+def ios_scan():
+   installed_apps = subprocess.run(['mobiledevice', 'list_apps'], stdout=subprocess.PIPE) 
    if (installed_apps.returncode != 0):
        print("Error running iOS device scan. Is the 'https://github.com/imkira/mobiledevice' \
             'code installed on this Mac?")
@@ -53,7 +53,34 @@ def _dev_ios_scan():
    print(spyware_on_phone[spyware_on_phone == True])
    #print(spyware_on_phone.value_counts())
   
-#def android_scan(phone_id):
+def android_scan():
+   installed_apps = subprocess.run(['adb', 'shell', 'pm', 'list', 'packages', 
+       '-f', '|', 'sed', '-e', "'s/.*=//'", '|', 'sed', "'s/\r//g'", '|', 'sort'],
+       stdout=subprocess.PIPE) 
+   if (installed_apps.returncode != 0):
+       print("Error running iOS device scan. Is the 'https://github.com/imkira/mobiledevice' \
+            'code installed on this Mac?")
+       exit(1)
+   installed_apps = installed_apps.stdout
+   installed_apps = installed_apps.split('\n').tolist()
+
+   # NOTE: or use DB!
+   apps_df = pd.read_csv(config.APPS_LIST, index_col='appId')
+   apps_df['appId'] = apps_df.index
+   relevant_spyware_df = apps_df[apps_df['relevant']=='y']
+   
+   spyware_on_phone = relevant_spyware_df['appId'].isin(installed_apps)
+   
+   print('-'*80)
+   print('Apps Installed on Phone:')
+   print('-'*80)
+   print(installed_apps)
+   print('-'*80)
+   print('Spyware Apps Detected on Phone')
+   print('-'*80)
+   print(spyware_on_phone[spyware_on_phone == True])
+   #print(spyware_on_phone.value_counts())
+
 
 if __name__ == "__main__":
-    _dev_ios_scan()
+    android_scan()
