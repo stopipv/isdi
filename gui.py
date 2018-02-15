1e682652e3253728d49151adf2604f9bd0f4fd78
Rahul Chatterjee <rahul@cs.cornell.edu>
2018-02-15 08:48:36 -0500

    ~

--- gui.py
+++ gui.py
@@ -3,11 +3,11 @@ from subprocess import Popen
 from subprocess import PIPE
 import pandas as pd
 import os
-import sys
 
 root_folder = os.path.join(os.path.dirname(os.path.abspath(__name__)), '../')
 # TODO: Change this to final ML output
 SPY_APP_CSV = os.path.join(root_folder, 'data/crawl_random_1000_apps.csv')
+
 
 def find_spywares():
     res = Popen(
@@ -27,16 +27,18 @@ def find_spywares():
 
 
 def uninstall_package(button):
-    package = app.getListBox("list")[0]
-    res = Popen("adb uninstall " + package, stdout=PIPE, shell=True)
-    res = res.stdout.read()
-    if res == "Success\r\r\n":
-        app.removeListItem("list", package)
-    app.infoBox("uninstall_res", res)
+    with gui("Anti-IPS: Stop intiimate partner surveillance") as app:
+        package = app.getListBox("list")[0]
+        res = Popen("adb uninstall " + package, stdout=PIPE, shell=True)
+        res = res.stdout.read()
+        if res == "Success\r\r\n":
+            app.removeListItem("list", package)
+        app.infoBox("uninstall_res", res)
+
 
 def render_gui(spyware_list):
     with gui("Anti-IPS: Stop intiimate partner surveillance") as app:
-        app.setGeometry(500,500)
+        app.setGeometry(500, 500)
         app.setStretch("both")
         app.setSticky("nesw")
 
@@ -46,11 +48,13 @@ def render_gui(spyware_list):
         tools = ['DELETE']
         app.addToolbar(tools, uninstall_package, findIcon=True)
 
+
 def main():
     spyware_list = find_spywares()
     print("Spyware found", '\n'.join(spyware_list))
     # if len(sys.argv)>1 and sys.argv[1] == '-gui':
     render_gui(spyware_list)
+
 
 if __name__ == "__main__":
     main()
