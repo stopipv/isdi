3fcff0df420d41bf41d9c0f9e06f60bf62ece798
Rahul Chatterjee <rahul@cs.cornell.edu>
2018-04-05 22:53:24 -0400

    The regex is broken need to fix it, the MVC structure needs to be updated. The view looks okay, need lots of testing.

--- TODO.md
+++ TODO.md
@@ -7,3 +7,11 @@
 
 5. Search for appIds with spy
 6. IOS how to uninstall the apps. 
+
+
+
+
+## Data TODO
+1. A csv file containing all the `appIds`,
+`store`={'android', 'ios', 'offstpre'}, and `flag`={'dual-use', 'spyware'}
+2. A file containing `appId`, `name`, and `url` of all apps we have seen so far.
---
+++ blacklist.py
@@ -0,0 +1,32 @@
+"""
+This file have different ways to detect spyware apps add flags to them.
+1. "onstore-dual-use": onstore dual-use apps
+2. "onstore-spyware": onstore apps which are clearly spyware based on our analysis
+3. "offstore-spyware": offstore spyware apps
+4. "regex-spy": Regex based spyware detection
+"""
+import re
+import config
+import pandas as pd
+
+APP_FLAGS = pd.read_csv(config.APP_FLAGS_FILE)
+SPY_REGEX = r'(?i)(?!anti)[\-\s]*(spy|track|keylog)'
+
+
+def _regex_blacklist(app):
+    return ['regex-spy'] if re.search(SPY_REGEX, app) \
+        else []
+
+
+def flag_apps(apps, device=''):
+    """Flag a list of apps based on the APP_FLAGS obtained from the csv file, or spy regex flags"""
+    _td = (pd.DataFrame({'appId': apps}).set_index('appId')
+           .join(APP_FLAGS.set_index('appId'), how="left"))
+    flagged_apps = (_td['store'] + '-' + _td['flag']).fillna('').apply(lambda x: [x] if x else [])
+    print(flagged_apps)
+    return flagged_apps + flagged_apps.index.map(_regex_blacklist)
+
+
+def flag_app(app, device=''):
+    return flag_apps([app], device=device).iloc[0]
+
