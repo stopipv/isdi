44942b4b6a85de9f610d54ead2df0e3c8a22eb6e
Rahul Chatterjee <rahul@cs.cornell.edu>
2018-04-06 21:12:06 -0400

    Added some more functionalities

--- server.py
+++ server.py
@@ -50,41 +50,44 @@ def scan(device):
     ser = request.args.get('serial')
     print(">>>scan_device", device, ser, "<<<<<")
     sc = get_device(device)
-    return json.dumps({
-        'apps': sc.find_spyapps(serialno=ser).to_json(orient="index"),
-        'serial': ser,
-        'error': config.error()
-    })
+    # return json.dumps({
+    #     'apps': sc.find_spyapps(serialno=ser).to_json(orient="index"),
+    #     'serial': ser,
+    #     'error': config.error()
+    # })
+    return render_template(
+        'main.html',
+        apps=sc.find_spyapps(serialno=ser).fillna('').to_dict(orient='index'),
+        serial=ser,
+        error=config.error()
+    )
 
 
-@FLASK_APP.route("/delete/<device>", methods=["PUT"])
+@FLASK_APP.route("/delete/<device>", methods=["GET"])
 def delete_app(device):
     sc = get_device(device)
     serial = request.args.get('serial')
     appid = request.args.get('appid')
+    # TODO: Record the uninstall and note
+    sc.save('app_uninstall', serial=serial, appid=appid, notes=request.args.get('notes', ''))
     return sc.uninstall(serialno=serial, appid=appid)
 
 
-@FLASK_APP.route('/submit', methods=["POST"])
-def record_response():
+@FLASK_APP.route('/submit/<device>', methods=["POST"])
+def record_response(device):
     if request.method == "POST":
-        t = re.match('(\w+)\?serial=(\w+)', request.form.get('url', ''))
-        if not t:
-            print(request.form)
-            return "The form could not be submitted!", 400
-        device, serial = t.groups()
-        app_feedback = dict(
-            (k.split('-', 1)[1], v)
-            for k, v in request.form.items()
-            if k != 'url'
-        )
+        # t = re.match('(\w+)\?serial=(\w+)', request.form.get('url', ''))
+        # if not t:
+        #     print(request.form)
+        #     return "The form could not be submitted!", 400
+        # device, serial = t.groups()
+        # TODO: Dump the whole form
         sc = get_device(device)
-        r = True
-        for k, v in app_feedback.items():
-            if v == 'delete':
-                print("Deleting {}".format(k))
-                r &= sc.uninstall(appid=k, serialno=serial)
-        r &= bool(sc.save(feedback=json.dumps(app_feedback), serial=serial))
+        r = bool(sc.save(
+            'response',
+            response=json.dumps(request.form),
+            serial=request.form.get('device-id'),
+        ))
         if not r:
             return "The action failed for some reason. See the logs "\
                 "in the terminal.", 401
@@ -92,7 +95,6 @@ def record_response():
             return "Success!!", 200
     else:
         return "hello", 200
-
 
 
 if __name__ == "__main__":
