b3021682589257ef5b4f19bb4b3beb8d47c24890
Samuel Havron <sgh7cc@virginia.edu>
2018-10-08 00:58:21 -0400

    Added third party permission parsing

--- tests/parse_ios_apps.py
+++ tests/parse_ios_apps.py
@@ -4,49 +4,65 @@ from functools import reduce
 import operator
 import json
 
-# load permissions mappings, apps plist
+# load permissions mappings and apps plist
 with open('ios_permissions.json', 'r') as fh:
     PERMISSIONS_MAP = json.load(fh)
-APPS_PLIST = plistlib.readPlist('iphone_plist.xml')
+APPS_PLIST = readPlist('iphone_plist.plist')
 
 def _retrieve(dict_, nest):
     '''
         Navigates dictionaries like dict_[nest0][nest1][nest2]...
+        gracefully.
     '''
     try:
         return reduce(operator.getitem, nest, dict_)
     except KeyError as e:
         return ""
 
-def _print_permissions(permissions, msg):
+def _check_unseen_permissions(permissions):
     for permission in permissions:
         if permission not in PERMISSIONS_MAP:
-            # add newly-discovered permission
+            print('Have not seen '+str(permission)+' before. Making note of this...')
+            permission_human_readable = permission.replace('kTCCService','')
             with open('ios_permissions.json', 'w') as fh:
-                PERMISSIONS_MAP[permission] = permission[11:]
+                PERMISSIONS_MAP[permission] = permission_human_readable
                 fh.write(json.dumps(PERMISSIONS_MAP))
-            permission = permission[11:]
-        print("\t{}: ".format(msg)+str(PERMISSIONS_MAP[permission]))
+            print('Noted.')
+        #print('\t'+msg+": "+str(PERMISSIONS_MAP[permission])+"\tReason: "+app.get(permission,'system app'))
 
-def permissions():
-    # get xml dump from ios_dump.sh
+def get_permissions(app):
+    '''
+        Returns a list of tuples (permission, developer-provided reason for permission).
+        Could modify this function to include whether or not the permission can be adjusted
+        in Settings.
+    '''
+    system_permissions = _retrieve(app, ['Entitlements','com.apple.private.tcc.allow'])
+    adjustable_system_permissions =  _retrieve(app, ['Entitlements','com.apple.private.tcc.allow.overridable'])
+    third_party_permissions = list(set(app) & set(PERMISSIONS_MAP))
+    _check_unseen_permissions(list(system_permissions)+list(adjustable_system_permissions))
+    
+    # (permission used, developer reason for requesting the permission)
+    all_permissions = list(set(map(lambda x: \
+            (PERMISSIONS_MAP[x], app.get(x, "permission granted by system")),\
+            list(set(system_permissions) | \
+            set(adjustable_system_permissions) | set(third_party_permissions)))))
+    pii = _retrieve(app, ['Entitlements','com.apple.private.MobileGestalt.AllowedProtectedKeys'])
+    #print("\tPII: "+str(pii))
+
+    return all_permissions
+
+def parse_dump():
+    # get plist dump from ios_dump.sh
     for app in APPS_PLIST:
         party = app["ApplicationType"].lower()
         if party in ['system','user']:
             print(app['CFBundleName'],"("+app['CFBundleIdentifier']+") is a {} app and has permissions:"\
                     .format(party))
 
-            permissions = _retrieve(app, ['Entitlements','com.apple.private.tcc.allow'])
-            adjustable_permissions =  _retrieve(app, ['Entitlements','com.apple.private.tcc.allow.overridable'])
-            PII = _retrieve(app, ['Entitlements','com.apple.private.MobileGestalt.AllowedProtectedKeys'])
-
-            _print_permissions(permissions, "Built-in")
-            _print_permissions(adjustable_permissions, "Adjustable from settings")
-
-            if PII:
-                print("\tPII: "+str(PII))
-
-        print("")
+            permissions = get_permissions(app)
+            for permission in permissions:
+                print("\t"+str(permission[0])+"\tReason: "+permission[1])
+            print("")
 
 if __name__ == "__main__":
-    permissions()
+    parse_dump()
