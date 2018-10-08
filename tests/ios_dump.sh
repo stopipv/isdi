415ea4524a4a1cee5b1ba79cd521b7842de55e1a
Samuel Havron <sgh7cc@virginia.edu>
2018-10-08 02:47:18 -0400

    added extra iOS parsing info

--- tests/ios_dump.sh
+++ tests/ios_dump.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+serial=$(idevice_id -l 2>&1 | tail -n 1)
+mkdir -p "$serial"
+cd $serial
 # gets all of the details about each app (basically what ios_deploy does but with extra fields)
-ideviceinstaller -l -o xml -o list_all > iphone_plist.plist
+ideviceinstaller -l -o xml -o list_all > ios_apps.plist
 # use with installer_parse.py
 
 # gets OS version, serial, etc. -x for xml. Raw is easy to parse, too.
-ideviceinfo -x
+ideviceinfo -x > ios_info.xml
 
 # try to check for jailbroken by mounting the entire filesystem. 
 # Gets output:
@@ -12,5 +15,7 @@ ideviceinfo -x
 # This service enables access to the root filesystem of your device.
 # Your device needs to be jailbroken and have the AFC2 service installed."
 # if fails (so in that case not jailbroken -- or 'not sure' for false negative).
-mkdir /tmp/mnt
-ifuse --root /tmp/mnt
+rm -rf /tmp/phonescanmnt
+mkdir -p /tmp/phonescanmnt
+ifuse --root /tmp/phonescanmnt &> ios_jailbroken.log
+cd ..
