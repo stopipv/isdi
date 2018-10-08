3a712392506d30f25331dc09701fb691055347c2
Samuel Havron <sgh7cc@virginia.edu>
2018-10-08 02:58:30 -0400

    serial

--- tests/ios_dump.sh
+++ tests/ios_dump.sh
@@ -3,11 +3,11 @@ serial=$(idevice_id -l 2>&1 | tail -n 1)
 mkdir -p "$serial"
 cd $serial
 # gets all of the details about each app (basically what ios_deploy does but with extra fields)
-ideviceinstaller -l -o xml -o list_all > ios_apps.plist
+ideviceinstaller -u "$serial" -l -o xml -o list_all > ios_apps.plist
 # use with installer_parse.py
 
 # gets OS version, serial, etc. -x for xml. Raw is easy to parse, too.
-ideviceinfo -x > ios_info.xml
+ideviceinfo -u "$serial" -x > ios_info.xml
 
 # try to check for jailbroken by mounting the entire filesystem. 
 # Gets output:
@@ -17,5 +17,5 @@ ideviceinfo -x > ios_info.xml
 # if fails (so in that case not jailbroken -- or 'not sure' for false negative).
 rm -rf /tmp/phonescanmnt
 mkdir -p /tmp/phonescanmnt
-ifuse --root /tmp/phonescanmnt &> ios_jailbroken.log
+ifuse -u "$serial" --root /tmp/phonescanmnt &> ios_jailbroken.log
 cd ..
