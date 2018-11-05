3afac2b3ac17c948dde23a18a8fb873b4473d2b6
havron <sgh65@cornell.edu>
2018-11-05 18:27:49 -0500

    new schema

--- schema.sql
+++ schema.sql
@@ -1,35 +1,50 @@
+CREATE TABLE IF NOT EXISTS clients (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  clientid TEXT,
+  location TEXT,
+  issues TEXT, -- JSON dump of checkboxed answers
+  assessment TEXT,
+  plan TEXT,
+  the_rest TEXT, -- if structure changes, keep json dump in here
+	time DATETIME DEFAULT (datetime('now', 'localtime'))
+);
+
 CREATE TABLE IF NOT EXISTS scan_res (
-       	id INTEGER PRIMARY KEY AUTOINCREMENT,
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
 	clientid TEXT,
 	serial TEXT,
 	note TEXT,
 	device TEXT,
-        location TEXT, -- currently empty
-	time DATETIME DEFAULT (datetime('now', 'localtime'))
+  device_primary_user TEXT,
+  device_access TEXT, -- JSONdump of person, frequency of access
+  how_obtained TEXT,
+  device_model TEXT,
+  device_version TEXT,
+  device_manufacturer TEXT,
+  battery_avg_last_charged INTEGER, -- take out maybe
+  last_charged DATETIME,
+	time DATETIME DEFAULT (datetime('now', 'localtime')),
+	FOREIGN KEY(clientid) REFERENCES clients(clientid)
 );
 
-
--- This is a backup table to keep logs of everything. 
--- CREATE TABLE IF NOT EXISTS all_logs (
---        id INTEGER PRIMARY KEY AUTOINCREMENT,
---        args TEXT,
---        formargs TEXT,
---        httpmethod TEXT,
---        res TEXT,
---        time DATETIME DEFAULT (datetime('now', 'localtime'))
--- );
-
-CREATE TABLE IF NOT EXISTS  app_info (
+CREATE TABLE IF NOT EXISTS app_info (
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
 	scanid INTEGER,
 	appid TEXT,
-        flags TEXT,
+  flags TEXT,
 	remark TEXT,
-        action_taken TEXT,
+  action_taken TEXT,
+  apk_path TEXT,
+  install_date DATETIME,
+  last_updated DATETIME,
+  app_version TEXT,
+  data_usage INTEGER, -- Mobile total received: Wifi-Total
+  battery_usage INTEGER, -- in mAh. show "not more than average app on your device" rather than actual amount on front-end.
 	time DATETIME DEFAULT (datetime('now', 'localtime')),
 	FOREIGN KEY(scanid) REFERENCES scan_res(id)
 );
-
+-- see also, how battery usage is measured on phone app itself in settings. percent of battery used by app.E
+-- https://stackoverflow.com/questions/45751387/how-do-i-calculate-the-battery-drain-for-a-particular-app-using-dumpsys-batterys
 CREATE INDEX IF NOT EXISTS idx_scan_res_clientid on scan_res  (clientid);
 CREATE INDEX IF NOT EXISTS idx_app_info_scanid on app_info  (scanid);
 
@@ -47,3 +62,13 @@ CREATE INDEX IF NOT EXISTS idx_app_info_scanid on app_info  (scanid);
 -- 	PRIMARY KEY (id)
 -- );
 --
+-- This is a backup table to keep logs of everything. 
+-- CREATE TABLE IF NOT EXISTS all_logs (
+--        id INTEGER PRIMARY KEY AUTOINCREMENT,
+--        args TEXT,
+--        formargs TEXT,
+--        httpmethod TEXT,
+--        res TEXT,
+--        time DATETIME DEFAULT (datetime('now', 'localtime'))
+-- );
+
