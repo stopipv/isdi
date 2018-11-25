d9dc52f83dae94624283a6599090bad08fe2b14f
Rahul Chatterjee <rahul@cs.cornell.edu>
2018-11-25 13:26:21 -0500

    Restructuring

---
+++ tests/test_parse_dump.py
@@ -0,0 +1,5 @@
+from parse_dump import AndroidDump, IosDump
+
+def test_android_dump():
+    ad = AndroidDump('./phone_dumps/83c6500a47585595f72d654829cab29edd2c4f5253e6c05d5576cf04661fd6eb_android.txt')
+    pass
