1aff7f5616202823a2c864029f771f4794b4d45d
Rahul Chatterjee <rahul@dell-large>
2018-12-03 17:09:32 -0500

    scripts/setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,36 +1,3 @@
-#!/usr/bin/env bash
-cd /tmp;
-# FIXME: if adb versions don't match, big problems. check first.
-command -v adb &>/dev/null
-if [[ "$?" -ne "0" ]]; then 
-    if [[ ! -e "$HOME/.platform-tools" ]]; then 
-        if [[ `uname` == "Linux" ]]; then 
-            echo "Installing Linux adb..."
-	    wget https://dl.google.com/android/repository/platform-tools-latest-linux.zip
-	    unzip -q platform-tools-latest-linux.zip
-        else
-            echo "Installing macOS adb..."
-	    wget https://dl.google.com/android/repository/platform-tools-latest-darwin.zip
-	    unzip -q platform-tools-latest-darwin.zip
-        fi
-	mv platform-tools "$HOME/.platform-tools"
-    fi
-    export ANDROID_HOME=$HOME/.platform-tools
-else
-    export ANDROID_HOME=$(dirname $(which adb))
-fi
+ln -sf static_data/app-flags.csv ./static_data/app-flags.csv~test
 
-cd -
-command -v ideviceinfo >/dev/null 2>&1 || ./ios_dependencies.sh
-#if [ ! -e ios-deploy/build/Release/ios-deploy ]; then
-#   git submodule update --recursive --remote && cd ios-deploy && xcodebuild && cd -
-#fi
-
-# Does not work for all versions of iOS
-# command -v mobiledevice &>/dev/null
-# if [[ "$?" -ne "0" ]]; then 
-#     git clone https://github.com/imkira/mobiledevice.git && \
-# 	cd ./mobiledevice && make && make install
-# fi
-
-export PATH=$PATH:$ANDROID_HOME
+ln -sf static_data/app-info.db ./static_data/app-info.db~test
