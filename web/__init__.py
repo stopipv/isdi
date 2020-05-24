f92939b5a4b95cf9099bd2200b6a8c971a7ba80a
Santiago Torres <santiago@archlinux.org>
2020-05-24 14:46:36 -0400

    web: move teardown methods into web
    These methods are useful for the isdi webapp application, and as such
    they would probably be better off living inside of that module. This
    finally leaves the isdi script to be a minimal, non-flask dependent
    application that can be used to dispatch mutliple ISDI-related
    workloads/targets.

--- web/__init__.py
+++ web/__init__.py
@@ -1,5 +1,8 @@
+from datetime import datetime, timedelta
+from time import strftime
+
 import config
-from flask import Flask
+from flask import Flask, g, session, request
 from flask_sqlalchemy import Model, SQLAlchemy
 from flask_migrate import Migrate
 
@@ -14,3 +17,37 @@ app.config['SESSION_TYPE'] = 'filesystem'
 sa=SQLAlchemy(app)
 Migrate(app, sa)
 # sa.create_all() # run in init_db()
+
+@app.before_request
+def make_session_permanent():
+    session.permanent = True
+    # expires at midnight of new day
+    app.permanent_session_lifetime = \
+            (datetime.now() + timedelta(days=1)).replace(hour=0, minute=0,
+                                                         second=0) - \
+                                                                 datetime.now()
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
+@app.after_request
+def after_request(response):
+    """ Logging after every request. """
+    # This avoids the duplication of registry in the log,
+    # since that 500 is already logged via @app.errorhandler.
+    if response.status_code != 500:
+        ts = strftime('[%Y-%b-%d %H:%M]')
+        logger.error('%s %s %s %s %s %s',
+                     ts,
+                     request.remote_addr,
+                     request.method,
+                     request.scheme,
+                     request.full_path,
+                     response.status)
+    return response
+
+
