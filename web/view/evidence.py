372a175485b718519de296c0105c5d66ea91bea6
Sophie Stephenson <sophia.r.stephenson@gmail.com>
2023-03-15 11:32:58 -0500

    collect suspicious apps and display as text

--- web/view/evidence.py
+++ web/view/evidence.py
@@ -1,7 +1,19 @@
-from flask import render_template, request
+import json
+import os
+
+from flask import redirect, render_template, request, session, url_for
 
 import config
+from db import (
+    create_mult_appinfo,
+    create_scan,
+    get_client_devices_from_db,
+    new_client_id,
+)
+from evidence_collection import app_list_to_str, get_suspicious_apps
 from web import app
+from web.view.index import get_device
+from web.view.scan import first_element_or_none
 
 
 @app.route("/evidence", methods=['GET'])
@@ -12,5 +24,20 @@ def evidence():
     return render_template(
         'main.html', task="evidence",
         device_primary_user=config.DEVICE_PRIMARY_USER,
-        title=config.TITLE
+        title=config.TITLE,
+        apps={}
     )
+
+@app.route("/evidence/apps", methods=['GET'])
+def evidence_apps():
+    """
+    TODO: Get list of apps which ISDi flags as dual-use or spyware
+    """
+    
+    # TODO get input from the user
+    device = 'ios'
+    device_owner = 'test'
+
+    app_list = get_suspicious_apps(device, device_owner)
+
+    return app_list_to_str(app_list)
