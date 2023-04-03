7dbe5c377fdfe9eac60acc03f40be73713d01fdb
Sophie Stephenson <sophia.r.stephenson@gmail.com>
2023-04-03 12:46:20 -0500

    use multi-page form data

--- web/view/evidence.py
+++ web/view/evidence.py
@@ -2,83 +2,91 @@ import json
 import os
 from pprint import pprint
 
-from flask import redirect, render_template, request, session, url_for
+from flask import Flask, redirect, render_template, request, session, url_for
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import SelectField, StringField, SubmitField, TextAreaField
+from wtforms.validators import InputRequired
 
 import config
-from db import (
-    create_mult_appinfo,
-    create_scan,
-    get_client_devices_from_db,
-    new_client_id,
-)
-from evidence_collection import get_suspicious_apps
 from web import app
-from web.view.index import get_device
-from web.view.scan import first_element_or_none
 
+bootstrap = Bootstrap(app)
 
-@app.route("/evidence", methods=['GET', 'POST'])
-def evidence():
+yes_no_choices = [('y', 'Yes'), ('n', 'No'), ('u', 'Unsure')]
+device_type_choices=[('android', 'Android'), ('ios', 'iOS')]
+
+class StartForm(FlaskForm):
+    title = "Welcome to <Name of tool>"
+    name = StringField('Name', validators=[InputRequired()])
+    device_type = SelectField('Device type:', choices=device_type_choices, validators=[InputRequired()])
+    submit = SubmitField("Continue")
+
+class SpywareForm(FlaskForm):
+    title = "Spyware Check"
+    knew_installed = SelectField('Did you know this app was installed?', choices=yes_no_choices, validators=[InputRequired()])
+    installed = SelectField('Did install this app?', choices=yes_no_choices, validators=[InputRequired()])
+    coerced = SelectField('Were you coerced into installing this app?', choices=yes_no_choices, validators=[InputRequired()])
+    submit = SubmitField("Continue")
+
+@app.route("/evidence/<int:step>", methods=['GET', 'POST'])
+def evidence(step):
     """
     TODO: Evidence stuff!
     """
+
+    forms = {
+        1: StartForm(),
+        2: SpywareForm(),
+    }
+
+    form = forms.get(step, 1)
+
+    if request.method == 'POST':
+        if form.is_submitted() and form.validate():
+            session['step{}'.format(step)] = form.data
+            if step < len(forms):
+                # Redirect to next step
+                return redirect(url_for('evidence', step=step+1))
+            else:
+                # Redirect to finish
+                return redirect(url_for('evidence_summary'))
+            
+    # If form data for this step is already in the session, populate the form with it
+    if 'step{}'.format(step) in session:
+        form.process(data=session['step{}'.format(step)])
+
     context = dict(
         task = "evidence",
+        progress =  int(step / len(forms) * 100),
+        step = step,
+        form = form,
         device_primary_user=config.DEVICE_PRIMARY_USER,
         title=config.TITLE,
         device_owner = "",
         device = "",
         scanned=False
     )
-
-    if request.method=="POST":
-
-        # get form data
-        device_owner = request.form["name"]
-        device = request.form["device"]
-
-        # get list of suspicious apps
-        apps = get_suspicious_apps(device, device_owner)
-        dual_use = []
-        spyware = []
-        for app in apps:
-            if "dual-use" in app["flags"]:
-                dual_use.append(app)
-            if "spyware" in app["flags"]:
-                spyware.append(app)
-
-        context.update(dict(
-            dual_use=dual_use,
-            spyware=spyware,
-            device_owner=device_owner,
-            device=device,
-            scanned=True
-        ))
-
+    
     return render_template('main.html', **context)
 
-@app.route("/evidence/summary", methods=['POST'])
+@app.route("/evidence/summary", methods=['GET'])
 def evidence_summary():
 
-    snapchat = dict(
-        recognize = request.form["recognizesnapchat"],
-        installed = request.form["installedsnapchat"],
-        installer = request.form["installersnapchat"],
-    )
-
-    app_summaries = [snapchat]
-
     context = dict(
-        task="evidencesummary",
+        task = "evidencesummary",
         device_primary_user=config.DEVICE_PRIMARY_USER,
         title=config.TITLE,
-        name="Sophie",
-        dual_use=request.form["dual_use"],
-        spyware=request.form["spyware"],
-        app_summaries = app_summaries
+        device_owner = "",
+        device = "",
+        scanned=False,
+        data = {},
     )
 
-    pprint(context["dual_use"])
-
+    for key in session.keys():
+        if key.startswith('step'):
+            context['data'].update(session[key])
+    session.clear()
 
     return render_template('main.html', **context)
+
