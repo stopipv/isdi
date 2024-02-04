a7592fc7c8a5b7725eb3f8458917f86ddbfad7a4
Rahul Chatterjee <rahul@yoga-laptop>
2024-02-04 13:26:17 -0600

    Fixing small typos in cofig.py

--- config.py
+++ config.py
@@ -2,20 +2,30 @@ from pathlib import Path
 import os
 import shlex
 from sys import platform
-from runcmd import run_command, catch_err
 import hashlib
 import hmac
+import secrets
 
 import logging
 import logging.handlers as handlers
 
 def setup_logger():
+    """
+    Set up a logger with a rotating file handler.
+
+    The logger will write in a file named 'app.log' in the 'logs' directory.
+    The log file will rotate when it reaches 100,000 bytes, keeps a maximum of 30 files.
+
+    Returns:
+        logging.Logger: The configured logger object.
+    """
     handler = handlers.RotatingFileHandler(
         'logs/app.log', maxBytes=100000, 
         backupCount=30)
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.INFO)
     logger.addHandler(handler)
+    return logger
 
 DEV_SUPPRTED = ['android', 'ios']    # 'windows', 'mobileos', later
 THIS_DIR = Path(__file__).absolute().parent
@@ -26,7 +36,7 @@ source_files = {
     'appstore': 'static_data/ios_apps_crawl.csv.gz',
     'offstore': 'static_data/offstore_apks.csv',
 }
-spyware_list_file = 'static_data/spyware.csv'   # hand picked
+SPYWARE_LIST_FILE = 'static_data/spyware.csv'   # hand picked
 
 # ---------------------------------------------------------
 DEBUG = bool(int(os.getenv("DEBUG", "0")))
@@ -47,8 +57,8 @@ IOS_DUMPFILES = {'Jailbroken-FS': 'ios_jailbroken.log',
 
 TEST_APP_LIST = 'static_data/android.test.apps_list'
 #TITLE = "Anti-IPS: Stop Intimate Partner Surveillance"
-
-TITLE = {'title': "IPV Spyware Discovery (ISDi){}".format(" (test)" if TEST else '')}
+tt_str = ' (test)' if TEST else ''
+TITLE = {'title': "IPV Spyware Discovery (ISDi){tt_str}"}
 
 APP_FLAGS_FILE = 'static_data/app-flags.csv'
 APP_INFO_SQLITE_FILE = 'sqlite:///static_data/app-info.db'
@@ -60,26 +70,27 @@ IOC_FILE = os.path.join(IOC_PATH, "ioc.yaml")
 # we will resolve the database path using an absolute path to __FILE__ because
 # there are a couple of sources of truth that may disagree with their "path
 # relavitity". Needless to say, FIXME
-SQL_DB_PATH = "sqlite:///{}".format(str(THIS_DIR / "data/fieldstudy.db"))
+SQL_DB_PATH = f"sqlite:///{str(THIS_DIR / 'data/fieldstudy.db')}"
 #SQL_DB_CONSULT_PATH = 'sqlite:///data/consultnotes.db' + ("~test" if TEST else "")
 
-
 def set_test_mode(test):
-    global TEST, APP_FLAGS_FILE, SQL_DB_PATH
-    TEST = test
-    if TEST:
-        if not APP_FLAGS_FILE.endswith('~test'):
-            APP_FLAGS_FILE = APP_FLAGS_FILE + "~test"
-        if not SQL_DB_PATH.endswith('~test'):
-            SQL_DB_PATH = SQL_DB_PATH + "~test"
+    """
+    Sets the test mode to the given value and returns the new values of APP_FLAGS_FILE and SQL_DB_PATH.
+    """
+    app_flags_file, sql_db_path = APP_FLAGS_FILE, SQL_DB_PATH
+    if test:
+        if not app_flags_file.endswith('~test'):
+            app_flags_file = APP_FLAGS_FILE + "~test"
+        if not sql_db_path.endswith('~test'):
+            sql_db_path = sql_db_path + "~test"
     else:
-        if APP_FLAGS_FILE.endswith('~test'):
-            APP_FLAGS_FILE = APP_FLAGS_FILE.replace("~test", '')
-        if SQL_DB_PATH.endswith('~test'):
-            SQL_DB_PATH = SQL_DB_PATH.replace("~test", '')
+        if app_flags_file.endswith('~test'):
+            app_flags_file = app_flags_file.replace("~test", '')
+        if sql_db_path.endswith('~test'):
+            sql_db_path = sql_db_path.replace("~test", '')
+    return app_flags_file, sql_db_path
 
-
-set_test_mode(TEST)
+APP_FLAGS_FILE, SQL_DB_PATH = set_test_mode(TEST)
 
 
 STATIC_DATA = THIS_DIR / 'static_data'
@@ -111,8 +122,17 @@ APPROVED_INSTALLERS = {
 REPORT_PATH = THIS_DIR / 'reports'
 PII_KEY_PATH = STATIC_DATA / "pii.key"
 def open_or_create_random_key(fpath, keylen=32):
+    """
+    Opens the file at the given path or creates a new file with a random key of the specified length.
+
+    Args:
+        fpath (str): The path to the file.
+        keylen (int, optional): The length of the random key. Defaults to 32.
+
+    Returns:
+        bytes: The contents of the file as bytes.
+    """
     def create():
-        import secrets
         with fpath.open('wb') as f:
             f.write(secrets.token_bytes(keylen))
 
@@ -120,7 +140,7 @@ def open_or_create_random_key(fpath, keylen=32):
         create()
     k = fpath.open('rb').read(keylen)
     if len(k) != keylen:
-        creatte()
+        create()
     return fpath.open('rb').read()
 
 PII_KEY = open_or_create_random_key(PII_KEY_PATH, keylen=32)
@@ -142,7 +162,6 @@ def hmac_serial(ser: str) -> str:
     return f'HSN_{hser}'
 
 def add_to_error(*args):
-    global ERROR_LOG
     m = '\n'.join(str(e) for e in args)
     print(m)
     ERROR_LOG.append(m)
@@ -154,5 +173,5 @@ def error():
     if len(ERROR_LOG) > 0:
         e, ERROR_LOG = ERROR_LOG[0], ERROR_LOG[1:]
 
-        print("ERROR: {}".format(e))
+        print(f"ERROR: {e}")
     return e.replace("\n", "<br/>")
