b2959ead69a969b5ac01ebf4b6caf8858a35d731
Rahul Chatterjee <rahul@yoga-laptop>
2025-03-09 23:55:00 -0500

    Trying to fix linting issues

--- scripts/android_scan.sh
+++ scripts/android_scan.sh
@@ -42,7 +42,6 @@ hmac_serial="-s $3"
 dump_dir="./phone_dumps/"
 ofname=$dump_dir/${hmac_serial:3}_android.txt
 
-email="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$"
 function scan {
     act=$1
     $adb "$serial" shell dumpsys "$act" | \
@@ -69,21 +68,21 @@ function retrieve {
     fi
     app="$1"
     process_uid=$(grep -A1 "Package \[$app\]" $ofname | sed '1d;s/.*userId=\([0-9]\+\).*/\1/g')
-    process_uidu=$(grep -Eo "app=ProcessRecord{[a-f0-9]+ [0-9]+:$app/([a-z0-9]*)" $ofname  | cut -d '/' -f 2 | sort -u)
+    process_uidu=$(grep -Eo "app=ProcessRecord{[a-f0-9]+ [0-9]+:$app/([a-z0-9]*)" "$ofname" | cut -d '/' -f 2 | sort -u)
     
     echo "$process_uid  $process_uidu"
     # Install date
-    printf 'Install: %s\n' "$(awk "/Package \[$app\]/{flag=1;next}/install permissions:/{flag=0}flag" $ofname | grep Time | tr -d '  ')"
+    printf 'Install: %s\n' "$(awk "/Package \[$app\]/{flag=1;next}/install permissions:/{flag=0}flag" "$ofname" | grep Time | tr -d '  ')"
 
     # Memory info 
-    printf 'Memory: %s\n' "$(awk '/Total PSS by process/{flag=1;next}/Total PSS/{flag=0}flag' $ofname | grep $app | sed '/^\s*$/d')"
+    printf 'Memory: %s\n' "$(awk '/Total PSS by process/{flag=1;next}/Total PSS/{flag=0}flag' "$ofname" | grep $app | sed '/^\s*$/d')"
 
     # Network info - cnt_set=0 => background,rx_bytes
-    printf 'DataUsage (%s): %s\n' "${process_uid}" "$(awk "/DUMP OF SERVICE net_stats/{flag=1;next}/DUMP OF SERVICE/{flag=0}flag" $ofname | sed 's/ /,/g' | csvgrep -c 4 -m ${process_uid} | csvcut -c 4,5,6,8)"
+    printf 'DataUsage (%s): %s\n' "${process_uid}" "$(awk "/DUMP OF SERVICE net_stats/{flag=1;next}/DUMP OF SERVICE/{flag=0}flag" "$ofname" | sed 's/ /,/g' | csvgrep -c 4 -m ${process_uid} | csvcut -c 4,5,6,8)"
     # awk "/DUMP OF SERVICE net_stats/{flag=1;next}/DUMP OF SERVICE/{flag=0}flag" $ofname | sed 's/ /,/g' | grep -a "${process_uid}"
 
     # Battery info
-    printf "Battery (%s): %s\n" "${process_uidu}" "$(awk '/Estimated power use/{flag=1;next}/^\s*$/{flag=0}flag' $ofname | grep "Uid ${process_uidu}")"
+    printf "Battery (%s): %s\n" "${process_uidu}" "$(awk '/Estimated power use/{flag=1;next}/^\s*$/{flag=0}flag' "$ofname" | grep "Uid ${process_uidu}")"
     # Device Admin
 }
 
@@ -94,7 +93,7 @@ services=(package location media.camera netpolicy mount
           activity appops)
 
 function dump {
-    for a in ${services[*]}; do
+    for a in "${services[@]}"; do
         echo "DUMP OF SERVICE $a"
         scan "$a"
     done
@@ -127,7 +126,7 @@ if [[ "$1" == "scan" ]]; then
     $adb $serial shell pm clear com.android.settings
 elif [[ "$1" == "info" ]]; then
     (>&2 echo "------ Running app info ------- $2 $3")
-    retrieve $3
+    retrieve "$3"
 else
     echo "$ bash $0 <scan|info> <serial_no> [appId]"
     exit 1;
