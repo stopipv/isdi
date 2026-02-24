93af172ec8f56dabbcb4faf1b6b5e3d6557915bc
Rahul Chatterjee <rahul@yoga-laptop>
2026-02-24 09:09:05 -0600

    Releasing 1.0.5

--- src/isdi/__init__.py
+++ src/isdi/__init__.py
@@ -6,7 +6,7 @@ A privacy and security scanner for mobile devices
 import sys
 import types
 
-__version__ = "1.0.4"
+__version__ = "1.0.5"
 __author__ = "ISDI Contributors"
 
 from isdi.config import get_config, get_data_dir, get_config_dir
